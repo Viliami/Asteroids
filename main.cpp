5cd0409bb6286748b21d2311631151c9fd5523e5
Viliami <viliamiuchiha@gmail.com>
2015-10-29 22:48:02 +0000

    Updated main.cpp

--- main.cpp
+++ main.cpp
@@ -5,14 +5,18 @@
 
 int numVerts;
 GLfloat *vertexArray, *normalArray, *uvArray;
+float rotation = 0.0f;
 
 void draw(){
     glLoadIdentity();
     gluLookAt(
-    0,-10,-10,//position
-    0,0,0,//center
+    0,0,1,//position
+    0,15,0.99f,//center
     0,1,0//up
     );
+    glScalef(0.5f,0.5f,0.5f);
+    glRotatef(180,0,0,1);
+    glRotatef(rotation,0,1,0);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     //glBindTexture(GL_TEXTURE_2D,texture[textureNumber]);
     glEnableClientState(GL_VERTEX_ARRAY);
@@ -27,6 +31,7 @@ void draw(){
     glDisableClientState(GL_VERTEX_ARRAY);
     glDisableClientState(GL_NORMAL_ARRAY);
     //glDisableClientState(GL_TEXTURE_COORD_ARRAY);
+    //rotation+=1.0f;
     glutSwapBuffers();
 }
 
@@ -37,7 +42,7 @@ void resize(int w, int h){
     glLoadIdentity();
     glMatrixMode(GL_PROJECTION);
     glViewport(1,1,w,h);
-    gluPerspective(45,ratio,1,100);
+    gluPerspective(90,ratio,0.1f,100);
     glMatrixMode(GL_MODELVIEW);
 }
 
@@ -58,26 +63,37 @@ void specialKeys(int key, int x, int y){
             printf("DOWN\n");
             break;
         case GLUT_KEY_LEFT:
-            printf("LEFT\n");
+            //printf("LEFT\n");
+            rotation += 2.0f;
             break;
         case GLUT_KEY_RIGHT:
-            printf("RIGHT\n");
+            //printf("RIGHT\n");
+            rotation -= 2.0f;
             break;
     }
 }
 
 void initGL(){
     loadOBJ("data/models/ship.dae",vertexArray,normalArray,uvArray,numVerts);
+    GLfloat light_position[] = { 1.0, 1.0, 1.0, 0.0 };
+    GLfloat light_ambient[] = { 0.3f, 0.3f, 0.3f, 0.0f };
+    glLightfv(GL_LIGHT0, GL_POSITION, light_position);
+    glLightfv(GL_LIGHT0, GL_AMBIENT, light_ambient);
+
+    glEnable(GL_LIGHTING);
+    glEnable(GL_LIGHT0);
+    glEnable(GL_DEPTH_TEST);
 }
 
 int main(int argc, char** argv){
     glutInit(&argc,argv);
     glutInitWindowPosition(-1,-1);
-    glutInitWindowSize(500,500);
+    glutInitWindowSize(700,700);
     glutInitDisplayMode(GLUT_RGBA | GLUT_DEPTH | GLUT_DOUBLE);
     glutCreateWindow("Asteroids");
 
     glutDisplayFunc(draw);
+    glutIdleFunc(draw);
     glutReshapeFunc(resize);
     glutKeyboardFunc(normalKeys);
     glutSpecialFunc(specialKeys);
