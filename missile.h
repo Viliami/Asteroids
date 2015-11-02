3c7a2fe2241518c6fb846030884d023671f0175a
Viliami <viliamiuchiha@gmail.com>
2015-11-02 03:46:12 +0000

    Updated missile.h

--- missile.h
+++ missile.h
@@ -1,8 +1,13 @@
+#include <GL/glut.h>
+#include "loaders.h"
 
 class Missile{
     public:
         Missile();
         ~Missile();
+        void draw();
     private:
-        
+        int numVerts;
+        GLfloat *vertexArray, *normalArray, *uvArray;
+        float rotation = 0.0f;
 };
